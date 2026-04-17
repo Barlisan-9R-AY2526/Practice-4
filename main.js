0236dcd9d20312bc2c89d387b2e057102032a64b
yanna <barlisan.al@obmontessori.edu.ph>
2026-04-17 13:20:20 +0800

    update main

---
+++ main.js
@@ -0,0 +1,11 @@
+function carChecker() {
+let carCounter = 18;
+const maxCars = 25;
+
+while(carCounter < maxCars) {
+    // window.alert(carNum);
+    window.alert("car no. " + carCounter + "is entering")
+carCounter += 1;
+}
+window.alert("Full Parking");
+}
